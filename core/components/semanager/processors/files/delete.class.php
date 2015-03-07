ca42fff7ab2ce25f4b0784a71c422ccdff8123d4
Florian Gutwald <florian@frontend-mercenary.com>
2015-03-07 17:00:39 +0100

    Optmize fileaction delete and CSS

--- core/components/semanager/processors/files/delete.class.php
+++ core/components/semanager/processors/files/delete.class.php
@@ -1,5 +1,24 @@
 <?php
-if($scriptProperties['path']){
-    unlink($scriptProperties['path']);
-    return $modx->error->success('',$item);
+
+
+if (!isset($modx->semanager) || !is_object($modx->semanager)) {
+    $semanager = $modx->getService('semanager','SEManager',$modx->getOption('semanager.core_path',null,$modx->getOption('core_path').'components/semanager/').'model/semanager/', $scriptProperties);
+    if (!($semanager instanceof SEManager)) return '---';
+}
+
+$file = $scriptProperties["file"];
+
+$result = true;
+
+if ($file) {
+    $result = $modx->semanager->deleteFile($file);
+} else {
+    $result = false;
+}
+
+
+if ($result == true) {
+    return $modx->error->success("");
+} else {
+    return $modx->error->failure("");
 }

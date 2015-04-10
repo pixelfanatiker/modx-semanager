eba413ed97481580e47145f85b2c90d105b7f16d
Florian <florian@frontend-mercenary.com>
2015-04-10 16:59:10 +0200

    Comment out constants

--- _build/build.config.php
+++ _build/build.config.php
@@ -6,7 +6,7 @@
 * @subpackage build
 */
 include_once dirname(dirname(__FILE__)) . '/config.core.php';
-define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
+/*define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
 define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
 define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'connectors/');
 define('MODX_ASSETS_PATH', MODX_BASE_PATH . 'assets/');
@@ -15,4 +15,4 @@ define('MODX_CONFIG_KEY','config');
 define('MODX_CORE_URL', MODX_BASE_URL . 'core/');
 define('MODX_MANAGER_URL', MODX_BASE_URL . 'manager/');
 define('MODX_CONNECTORS_URL', MODX_BASE_URL . 'connectors/');
-define('MODX_ASSETS_URL', MODX_BASE_URL . 'assets/');
+define('MODX_ASSETS_URL', MODX_BASE_URL . 'assets/');*/
